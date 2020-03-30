3baffd9b3f677f694f3ef2f34cfbb461a458509e
Preyansh Rastogi <44090539+prey176@users.noreply.github.com>
2020-03-31 03:26:31 +0530

    Update binarylifting2.cpp

--- binarylifting2.cpp
+++ binarylifting2.cpp
@@ -139,10 +139,10 @@ signed main ()
 {
 	#ifndef ONLINE_JUDGE
 	freopen("input.txt","r",stdin) ;
-    freopen("output.txt","w",stdout) ;
+    	freopen("output.txt","w",stdout) ;
 	#endif
 
-    fast_io();
+    	fast_io();
 	int t = 1 ;
 	// cin >> t ;
 	while (t--) solve() ;  
